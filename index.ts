ab4dacc630d573b3d4fb4c6e1403167d9a15e2b1
Kanika Gola <kanikagola26@gmail.com>
2022-07-12 16:05:10 +0530

    Published a sample SDK

---
+++ index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+const API_URL: string = 'https://pokeapi.co/api/v2';
+
+export function getPokemonById(id: number): Promise<object> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${API_URL}/pokemon/${id}`)
+        .then((resp) => {
+          resolve(resp.data);
+        })
+        .catch(reject);
+    });
+  }
+
+  export function getPokemonTypeById(id: number): Promise<object> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`${API_URL}/type/${id}`)
+        .then((resp) => {
+          resolve(resp.data);
+        })
+        .catch(reject);
+    });
+  }
+  
+  
+  export default { getPokemonById, getPokemonTypeById };
